746946f06e52a0b64e8e6dc934f9c2831c562e92
hallieping <37218334+hallieping@users.noreply.github.com>
2019-10-14 19:38:28 +0800

    Create 第N个丑数.java

---
+++ 第N个丑数.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+public class Solution {
+    public int GetUglyNumber_Solution(int index) {
+        if(index < 1) return 0;
+        ArrayList<Integer> list=new ArrayList<Integer>();
+        list.add(1);
+        int i2=0,i3=0,i5=0;
+        while(list.size()<index)//循环的条件
+        {
+            int m2=list.get(i2)*2;
+            int m3=list.get(i3)*3;
+            int m5=list.get(i5)*5;
+            int min=Math.min(m2,Math.min(m3,m5));
+            list.add(min);
+            if(min==m2)i2++;
+            if(min==m3)i3++;
+            if(min==m5)i5++;
+        }
+        return list.get(list.size()-1);
+    }
+}
+
+///质因数只有2,3,5. 循环乘积后比较传入
