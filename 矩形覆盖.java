fa171f52341c8173773c0993f3c83b5bf04ddb2b
hallieping <37218334+hallieping@users.noreply.github.com>
2019-10-20 20:32:48 +0800

    Update 矩形覆盖.java

--- 矩形覆盖.java
+++ 矩形覆盖.java
@@ -1,6 +1,6 @@
 //如果用1*m的方块覆盖m*n区域，递推关系式为f(n) = f(n-1) + f(n-m)，(n > m)。
 //解释：想成长n*m高的区域，和1*m的方块高相同，则往后增加时，第n个相当于由第n-2个增加2个1*m的块，第n-1个增加1个1*m的块
-
+//这里 m = 2 ！
 public class Solution {
     public int RectCover(int target) {
         if(target < 3) return target;
