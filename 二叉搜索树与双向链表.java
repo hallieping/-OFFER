82779610bac4e7a57f7fb041d44bec7ea43a2fe9
hallieping <37218334+hallieping@users.noreply.github.com>
2019-10-28 19:32:49 +0800

    Create 二叉搜索树与双向链表.java

---
+++ 二叉搜索树与双向链表.java
@@ -0,0 +1,33 @@
+
+//输入一棵二叉搜索树，将该二叉搜索树转换成一个排序的双向链表。要求不能创建任何新的结点，只能调整树中结点指针的指向
+
+链接：https://www.nowcoder.com/questionTerminal/947f6eb80d944a84850b0538bf0ec3a5
+来源：牛客网
+
+    import java.util.Stack;
+    public TreeNode ConvertBSTToBiList(TreeNode root) {
+        if(root==null)
+            return null;
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode p = root;
+        TreeNode pre = null;// 用于保存中序遍历序列的上一节点
+        boolean isFirst = true;
+        while(p!=null||!stack.isEmpty()){
+            while(p!=null){
+                stack.push(p);
+                p = p.left;
+            }
+            p = stack.pop();
+            if(isFirst){
+                root = p;// 将中序遍历序列中的第一个节点记为root
+                pre = root;
+                isFirst = false;
+            }else{
+                pre.right = p;
+                p.left = pre;
+                pre = p;
+            }      
+            p = p.right;
+        }
+        return root;
+    }
