03ec08e381f3b7d3e91a3c96c877f5f254628340
hallieping <37218334+hallieping@users.noreply.github.com>
2019-10-30 19:39:35 +0800

    Update 数据流中的中位数.java

--- 数据流中的中位数.java
+++ 数据流中的中位数.java
@@ -1,3 +1,5 @@
+
+
 import java.util.ArrayList;
 import java.util.Collections;
 public class Solution {
@@ -17,3 +19,29 @@ public class Solution {
         }
     }
 }
+/*
+链接：https://www.nowcoder.com/questionTerminal/9be0172896bd43948f8a32fb954e1be1
+来源：牛客网
+
+    private PriorityQueue<Integer> min = new PriorityQueue<Integer>();
+    private PriorityQueue<Integer> max = new PriorityQueue<Integer>(15,new Comparator<Integer>() {
+        public int compare(Integer o1, Integer o2) {
+            return o2.compareTo(o1);
+        }
+    });
+    private int count = 0;
+    public void Insert(Integer num) {
+        count++;
+        if (count % 2 == 1){
+            max.offer(num);//先从大顶堆过滤一遍
+            min.offer(max.poll());
+        }else {
+            min.offer(num);//先从小顶堆过滤一遍
+            max.offer(min.poll());
+        }
+    }
+    public Double GetMedian() {
+        if (count % 2 == 0)     return (min.peek() + max.peek())/2.0;
+        else                    return (double)min.peek();
+    }
+    */
