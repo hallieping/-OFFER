c548d5a98ccb1557bd1c9596fadd7c0651071ea0
hallieping <37218334+hallieping@users.noreply.github.com>
2019-12-05 09:07:47 +0800

    Update and rename 整数中1出现的次数 to 整数中1出现的次数.java

--- 整数中1出现的次数.java
+++ 整数中1出现的次数.java
@@ -24,3 +24,23 @@ public class Solution {
     return total;       
     }
 }
+
+//另外：
+//链接：https://www.nowcoder.com/questionTerminal/bd7f978302044eee894445e244c7eee6
+//来源：牛客网
+
+ int NumberOf1Between1AndN_Solution(int n)
+    {
+        int ones = 0;
+        for (long long m = 1; m <= n; m *= 10) {
+            int a = n/m, b = n%m;
+            if(a%10 == 0)
+                ones += a / 10 * m;
+            else if(a%10 == 1) 
+                ones += (a/10*m) + (b+1);
+            else
+                ones += (a/10+1)* m;
+        }
+        return ones;
+    }
+
