068a3e074c8a9915a0c97d462d8b2d56c132c6e3
hallieping <37218334+hallieping@users.noreply.github.com>
2019-12-06 21:27:33 +0800

    Update 和为S的两个数字.java

--- 和为S的两个数字.java
+++ 和为S的两个数字.java
@@ -1,53 +1,27 @@
+//因为序列是增序的，所以其实序列两侧的乘积是最小的，按照左右夹逼即可
 import java.util.ArrayList;
 public class Solution {
-    public ArrayList<Integer> FindNumbersWithSum(int [] array,int sum) {
-        ArrayList<Integer> res = new ArrayList<>();
-        if(array.length<=1)
-            return res;   
-        int l = 0; 
-        int r = array.length-1;
-        int min = array[l]*array[r];
-        int s1=array[0];
-        int s2=array[r];
-        boolean real = false;
-        while(l < r) {
-            if(array[l] == sum - array[r]) {
-                min =array[l] * array[r];
-                s1 = array[l];
-                s2 = array[r];
-                break;
-            }
-            else if(array[l] < sum - array[r]) {
-                l++;
-                continue;
-            }
-            else {
-                r--;
+ public ArrayList<Integer> FindNumbersWithSum(int[] array, int sum) {
+        ArrayList<Integer> list = new ArrayList<>();
+        if (array == null || array.length == 0)
+            return list;
+        int left = 0;
+        int right = array.length - 1;
+        while (left < right) {
+            int total = array[left] + array[right];
+            if (total == sum) {
+                list.add(array[left]);
+                list.add(array[right]);
+                return list;
+            } else if (total > sum) {
+              //大于sum，说明太大了，right左移寻找更小的数
+                --right;
+                  
+            } else {
+              //2.如果和小于sum，说明太小了，left右移寻找更大的数
+                ++left;
             }
         }
-        while(l < r) {
-            if(array[l] == sum - array[r]) {
-                if(array[l] * array[r] < min) {
-                    min =array[l] * array[r];
-                    s1 = array[l];
-                    s2 = array[r];
-                }
-                l++;
-                r--;
-                real = true;
-            }
-            else if(array[l] < sum - array[r]) {
-                l++;
-                continue;
-            }
-            else {
-                r--;
-            }
-        }
-        if(real){
-            res.add(s1);
-            res.add(s2);
-        }
-        return res;
+        return list;
     }
 }
