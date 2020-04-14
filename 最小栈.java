4933cebc65b2dc7dffdca2681b0636aa5a07df92
hallieping <37218334+hallieping@users.noreply.github.com>
2020-04-14 09:10:14 +0800

    Update 最小栈.java

--- 最小栈.java
+++ 最小栈.java
@@ -70,3 +70,34 @@ public int top() {
 public int getMin() {
     return stack.peek();
 }
+
+// O（1）
+public class 设计一个有gitMin的栈 {
+
+    private Stack<Integer> stack = new Stack<Integer>();
+    private int min;
+
+    public void push(int x) {
+        if (stack.isEmpty()) {
+            min = x;
+            stack.push(0);
+        } else {
+            // 计算差值
+            int compare = x - min;
+            stack.push(compare);
+            // 如果差值小于0，显然 x 成为最小值，否则最小值不变
+            min = compare < 0 ? x : min;
+        }
+    }
+
+    public void pop() {
+        int top = stack.peek();
+        // 如果top小于0，显然最小值也一并会被删除，此时更新最小值
+        min = top < 0 ? (min - top) : min;
+        stack.pop();
+    }
+
+    public int getMin() {
+        return min;
+    }
+ }
